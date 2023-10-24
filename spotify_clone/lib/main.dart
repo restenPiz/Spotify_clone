6664f9f9b21a6bb3875914682af1c528278707bf
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-10-24 10:15:59 +0200

    Some updates in the application

--- spotify_clone/lib/main.dart
+++ spotify_clone/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Spotify App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 0, 0, 0)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Bom Dia'),
@@ -43,33 +43,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-
-            //*Inicio do texto que vai aparecer que vai aparecer na tela do applicativo
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-
-      //*Inicio do floating button responsavel por fazer o incremento
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      
     );
   }
 }
