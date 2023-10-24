9dc23bf1e6fe4724530819fe1aa55e92d104c731
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-10-24 10:33:50 +0200

    Some updates in the application

--- spotify_clone/lib/main.dart
+++ spotify_clone/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class Main extends StatefulWidget {
