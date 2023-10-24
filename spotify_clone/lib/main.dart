df8057a7540124d8625dacaba4c2b1ad10ba44a5
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-10-24 09:23:33 +0200

    Removing some unnecessary source code

--- spotify_clone/lib/main.dart
+++ spotify_clone/lib/main.dart
@@ -82,13 +82,9 @@ class _MyHomePageState extends State<MyHomePage> {
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
