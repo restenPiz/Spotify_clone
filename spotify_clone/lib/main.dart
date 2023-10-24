6f95f19313368d4d64287b34296cd0daeb31c5f0
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-10-24 11:54:13 +0200

    Some updates in the application

--- spotify_clone/lib/main.dart
+++ spotify_clone/lib/main.dart
@@ -42,7 +42,6 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Container(
