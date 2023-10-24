5bab6a1b38a92a55f0c9285bfc4d24d36759af56
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-10-24 11:23:47 +0200

    Some updates in the application

--- spotify_clone/lib/main.dart
+++ spotify_clone/lib/main.dart
@@ -56,6 +56,8 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
+
+            //?Inicio do metodo que printa o incremento
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
