daf062dc85e4938f34e9275ad5ae5c1266a37dca
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-10-24 10:20:37 +0200

    Some updates in the application

--- spotify_clone/lib/main.dart
+++ spotify_clone/lib/main.dart
@@ -34,21 +34,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-
-              //*Inicio do corpo da ap
-
-          ],
-        ),
-      ),
-    );
+    return Container();
   }
 }
