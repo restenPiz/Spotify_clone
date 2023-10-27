eff3eee463469e0dc789f80ddf3672c915f4283a
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-10-27 09:44:44 +0200

    Some updates in the application

--- spotify_clone/lib/main.dart
+++ spotify_clone/lib/main.dart
@@ -44,6 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           children: [
             Container(
+              //*Inicio do texto da application
               child: Text('Ola Mundo'),
             ),
           ],
