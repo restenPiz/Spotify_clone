f1ac5affd187f554ad73e60dd273ce0d5d9365d8
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-10-30 11:00:25 +0200

    Some updates in the application

--- spotify_clone/lib/Screen/carousel.dart
+++ spotify_clone/lib/Screen/carousel.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors
+
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 class Carousel extends StatefulWidget {
