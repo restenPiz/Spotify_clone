947d85395c7074af71347b80de51fbfa42efc0ec
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-10-30 08:48:06 +0200

    Some updates in the application

--- spotify_clone/lib/Screen/body.dart
+++ spotify_clone/lib/Screen/body.dart
@@ -14,11 +14,23 @@ class _BodyState extends State<Body> {
   Widget build(BuildContext context) {
     return Container(
       //*Inicio do corpo da app
-      child: Row(children: [
-        Container(
-          child: Text('Ola Mundo'),
-        )
-      ]),
+      child: Row(
+        children: [
+          Container(
+            child: Card(
+              // Customize a aparência do card usando o widget Card
+              elevation: 4, // Elevação para uma sombra sutil
+              margin: EdgeInsets.all(16), // Margem ao redor do card
+              child: ListTile(
+                leading: Icon(Icons.favorite), // Ícone à esquerda
+                title: Text('Título do Card'), // Título do card
+                subtitle: Text('Descrição do Card'), // Descrição do card
+                trailing: Icon(Icons.arrow_forward), // Ícone à direita
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
