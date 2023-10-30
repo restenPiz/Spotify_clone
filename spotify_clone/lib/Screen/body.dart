8eac9d01af4e45d6ddc905b38d57435250f8bf47
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-10-30 08:57:02 +0200

    Some updates in the application

--- spotify_clone/lib/Screen/body.dart
+++ spotify_clone/lib/Screen/body.dart
@@ -12,24 +12,14 @@ class Body extends StatefulWidget {
 class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      //*Inicio do corpo da app
-      child: Row(
-        children: [
-          Container(
-            child: Card(
-              color: Colors.white,
-              elevation: 4, // Elevação para uma sombra sutil
-              margin: EdgeInsets.all(16), // Margem ao redor do card
-              child: ListTile(
-                leading: Icon(Icons.favorite), // Ícone à esquerda
-                title: Text('Título do Card'), // Título do card
-                subtitle: Text('Descrição do Card'), // Descrição do card
-                trailing: Icon(Icons.arrow_forward), // Ícone à direita
-              ),
-            ),
-          ),
-        ],
+    return Card(
+      elevation: 4,
+      margin: EdgeInsets.all(16),
+      child: ListTile(
+        leading: Icon(Icons.favorite),
+        title: Text('Título do Card'),
+        subtitle: Text('Descrição do Card'),
+        trailing: Icon(Icons.arrow_forward),
       ),
     );
   }
