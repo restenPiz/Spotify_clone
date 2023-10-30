c94ba2f16b045faee085db71206ad586bc3d3005
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-10-30 11:01:55 +0200

    Some updates in the application

--- spotify_clone/lib/Screen/carousel.dart
+++ spotify_clone/lib/Screen/carousel.dart
@@ -14,6 +14,8 @@ class _CarouselState extends State<Carousel> {
   Widget build(BuildContext context) {
     return Container(
       child: Center(
+
+        //*Inicio do carousel
         child: CarouselSlider(
           items: [
             Container(
