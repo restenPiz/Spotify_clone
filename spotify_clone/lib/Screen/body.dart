32923ebc5705a7a4cf4d467848898c02cf76b425
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-10-30 12:07:08 +0200

    Some updates in the application

--- spotify_clone/lib/Screen/body.dart
+++ spotify_clone/lib/Screen/body.dart
@@ -13,158 +13,165 @@ class Body extends StatefulWidget {
 class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SingleChildScrollView(
       child: Column(
         children: [
-          Row(
-            children: [
-              //*Inicio das cards
-              Expanded(
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                  elevation: 4,
-                  margin: EdgeInsets.all(16),
-                  child: ListTile(
-                    //leading: Icon(Icons.favorite),
-                    title: Text('Musica'),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25), 
-                  ),
-                  elevation: 4,
-                  margin: EdgeInsets.all(16),
-                  child: ListTile(
-                    //leading: Icon(Icons.favorite),
-                    title: Text('Podcast e programas'),
-                  ),
-                ),
-              ),
-              Container(
-                height: 100,
-              ),
-              //*Fim das cards
-            ],
-          ),
-
-          //*Inicio da row que mostra as bibliotecas
-          Row(
-            children: [
-              //*Inicio das cards
-              Expanded(
-                child: Card(
-                  elevation: 4,
-                  margin: EdgeInsets.all(16),
-                  child: ListTile(
-                    leading: Image.asset('Images/a1.jpg'),
-                    title: Text('Musica de que gostaste'),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Card(
-                  elevation: 4,
-                  margin: EdgeInsets.all(16),
-                  child: ListTile(
-                    leading: Image.asset('Images/a3.jpg'),
-                    title: Text('Amapiano grooves'),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              //*Inicio das cards
-              Expanded(
-                child: Card(
-                  elevation: 4,
-                  margin: EdgeInsets.all(16),
-                  child: ListTile(
-                    leading: Image.asset('Images/a3.jpg'),
-                    title: Text('Catarse'),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Card(
-                  elevation: 4,
-                  margin: EdgeInsets.all(16),
-                  child: ListTile(
-                    leading: Image.asset('Images/a1.jpg'),
-                    title: Text('Ivy league'),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              //*Inicio das cards
-              Expanded(
-                child: Card(
-                  elevation: 4,
-                  margin: EdgeInsets.all(16),
-                  child: ListTile(
-                    leading: Image.asset('Images/a3.jpg'),
-                    title: Text('Mix romantica'),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Card(
-                  elevation: 4,
-                  margin: EdgeInsets.all(16),
-                  child: ListTile(
-                    leading: Image.asset('Images/a1.jpg'),
-                    title: Text('Em modo repeticao'),
-                  ),
-                ),
-              ),
-            ],
-          ),
-
-          //*Inicio do Titulo que aparece antes do Carousel
           Container(
-            height:20,
-          ),
-          Container(
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.only(left: 16),
-            child: Text('Feito para Mauro Peniel',
-              style: TextStyle(
-                fontSize: 22
-              ),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    //*Inicio das cards
+                    Expanded(
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        elevation: 4,
+                        margin: EdgeInsets.all(16),
+                        child: ListTile(
+                          //leading: Icon(Icons.favorite),
+                          title: Text('Musica'),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25), 
+                        ),
+                        elevation: 4,
+                        margin: EdgeInsets.all(16),
+                        child: ListTile(
+                          //leading: Icon(Icons.favorite),
+                          title: Text('Podcast e programas'),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 100,
+                    ),
+                    //*Fim das cards
+                  ],
+                ),
+
+                //*Inicio da row que mostra as bibliotecas
+                Row(
+                  children: [
+                    //*Inicio das cards
+                    Expanded(
+                      child: Card(
+                        elevation: 4,
+                        margin: EdgeInsets.all(16),
+                        child: ListTile(
+                          leading: Image.asset('Images/a1.jpg'),
+                          title: Text('Musica de que gostaste'),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Card(
+                        elevation: 4,
+                        margin: EdgeInsets.all(16),
+                        child: ListTile(
+                          leading: Image.asset('Images/a3.jpg'),
+                          title: Text('Amapiano grooves'),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    //*Inicio das cards
+                    Expanded(
+                      child: Card(
+                        elevation: 4,
+                        margin: EdgeInsets.all(16),
+                        child: ListTile(
+                          leading: Image.asset('Images/a3.jpg'),
+                          title: Text('Catarse'),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Card(
+                        elevation: 4,
+                        margin: EdgeInsets.all(16),
+                        child: ListTile(
+                          leading: Image.asset('Images/a1.jpg'),
+                          title: Text('Ivy league'),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    //*Inicio das cards
+                    Expanded(
+                      child: Card(
+                        elevation: 4,
+                        margin: EdgeInsets.all(16),
+                        child: ListTile(
+                          leading: Image.asset('Images/a3.jpg'),
+                          title: Text('Mix romantica'),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Card(
+                        elevation: 4,
+                        margin: EdgeInsets.all(16),
+                        child: ListTile(
+                          leading: Image.asset('Images/a1.jpg'),
+                          title: Text('Em modo repeticao'),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                //*Inicio do Titulo que aparece antes do Carousel
+                Container(
+                  height:20,
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.only(left: 16),
+                  child: Text('Feito para Mauro Peniel',
+                    style: TextStyle(
+                      fontSize: 22
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 20,
+                ),
+
+                //!Inicio da widget de carousel
+                Carousel(),
+
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.only(left: 16),
+                  child: Text('Feito para Mauro Peniel',
+                    style: TextStyle(
+                      fontSize: 22
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 20,
+                ),
+
+                //!Inicio da widget de carousel
+                Carousel(),
+                
+              ],
             ),
+      
           ),
-          Container(
-            height: 20,
-          ),
-
-          //!Inicio da widget de carousel
-          Carousel(),
-
-          Container(
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.only(left: 16),
-            child: Text('Feito para Mauro Peniel',
-              style: TextStyle(
-                fontSize: 22
-              ),
-            ),
-          ),
-          Container(
-            height: 20,
-          ),
-
-          //!Inicio da widget de carousel
-          Carousel(),
-          
         ],
       ),
     );
